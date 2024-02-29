eb613b466aa702ba9fe07ace0e2d830d4667087c
Hamel Desai <hamel.desai@axio.com>
2024-02-29 14:50:54 -0500

    section-8-video-61-converting-html-go-templates

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,10 @@ func routes(app *config.AppConfig) http.Handler {
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+	mux.Get("/generals-quarters", handlers.Repo.Generals)
+	mux.Get("/majors-suite", handlers.Repo.Majors)
+	mux.Get("/search-availability", handlers.Repo.Availability)
+	mux.Get("/contact", handlers.Repo.Contact)
 
 	// . is the root level directory of the app
 	fileServer := http.FileServer(http.Dir("./static/"))
