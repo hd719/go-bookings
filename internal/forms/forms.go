6d31e27a0fac683b9baa33cacb5f566f57fbb257
Hamel Desai <hamel.desai@axio.com>
2024-03-15 12:00:00 -0400

    section-9-video-80

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -2,7 +2,6 @@ package forms
 
 import (
 	"fmt"
-	"net/http"
 	"net/url"
 	"strings"
 
@@ -23,17 +22,6 @@ func New(data url.Values) *Form {
 	}
 }
 
-// Has check if form field is in post and not empty
-func (f *Form) Has(field string, r *http.Request) bool {
-	x := r.Form.Get(field)
-	if x == "" {
-		// f.Errors.Add(field, "This field cannot be blank")
-		return false
-	}
-
-	return true
-}
-
 // Check if the form is valid and we do this by checking the errors obj
 func (f *Form) Valid() bool {
 	return len(f.Errors) == 0
@@ -51,9 +39,20 @@ func (f *Form) Required(fields ...string) {
 	}
 }
 
+// Has check if form field is in post and not empty
+func (f *Form) Has(field string) bool {
+	x := f.Get(field)
+	if x == "" {
+		// f.Errors.Add(field, "This field cannot be blank")
+		return false
+	}
+
+	return true
+}
+
 // Checks for Min. length of string
-func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	x := r.Form.Get(field)
+func (f *Form) MinLength(field string, length int) bool {
+	x := f.Get(field)
 
 	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
