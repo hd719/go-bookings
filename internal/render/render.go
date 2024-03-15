2c3ba965481ae7eb2082f4927949750407a479d1
Hamel Desai <hamel.desai@axio.com>
2024-03-14 20:45:21 -0400

    section-9-video-77

--- internal/render/render.go
+++ internal/render/render.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"html/template"
 	"log"
@@ -29,7 +30,7 @@ func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateDa
 	return td
 }
 
-func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, data *models.TemplateData) {
+func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, data *models.TemplateData) error {
 
 	var tc map[string]*template.Template
 
@@ -41,7 +42,8 @@ func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, data *m
 
 	t, ok := tc[tmpl]
 	if !ok {
-		log.Fatal("Could not get template from template cache")
+		// log.Fatal("Could not get template from template cache")
+		return errors.New("cant get template from cache")
 	}
 
 	buf := new(bytes.Buffer)
@@ -53,7 +55,10 @@ func RenderTemplate(w http.ResponseWriter, r *http.Request, tmpl string, data *m
 	_, err := buf.WriteTo(w)
 	if err != nil {
 		log.Println("Error writing template to browser", err)
+		return err
 	}
+
+	return nil
 }
 
 func CreateTemplateCache() (map[string]*template.Template, error) {
