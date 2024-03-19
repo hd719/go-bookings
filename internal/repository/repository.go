3b1e038105fd0fe862dbd2b5147b5cdc6956a3ea
Hamel Desai <hamel.desai@axio.com>
2024-03-19 18:57:27 -0400

    section-13-video-107

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,14 @@
 package repository
 
-import "github.com/hd719/go-bookings/internal/models"
+import (
+	"time"
+
+	"github.com/hd719/go-bookings/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 	InsertReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesForRoom(start, end time.Time, roomId int) (bool, error)
 }
