fd05e1d20815b859b9e6bef30799167d84c47118
Hamel Desai <hamel.desai@axio.com>
2024-03-20 19:48:47 -0400

    section-13-video-117/8-databse-seeding-restrictions-and-rooms

---
+++ migrations/20240320234413_seed_rooms_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."rooms" ("id", "room_name", "created_at", "updated_at") VALUES
+(1, 'General''s Quarters', '2024-03-19 00:00:00', '2024-03-19 00:00:00'),
+(2, 'Major''s Suite', '2024-03-19 00:00:00', '2024-03-19 00:00:00');
---
+++ migrations/20240320234642_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."restrictions" ("id", "restriction_name", "created_at", "updated_at") VALUES
+(1, 'Reservation', '2024-03-10 00:00:00', '2024-03-10 00:00:00'),
+(2, 'Owner Block', '2024-03-20 00:00:00', '2024-03-20 00:00:00');
