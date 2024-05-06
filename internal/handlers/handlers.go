791ac143a7617809bade313079d48b348d8509c0
Hamel Desai <hameldesai93@gmail.com>
2024-05-06 13:12:59 -0400

    section-17-video-153

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -10,6 +10,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/go-chi/chi"
 	"github.com/hd719/go-bookings/internal/config"
 	"github.com/hd719/go-bookings/internal/driver"
 	"github.com/hd719/go-bookings/internal/forms"
@@ -542,10 +543,6 @@ func (m *Repository) AdminShowReservation(w http.ResponseWriter, r *http.Request
 	})
 }
 
-func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
-	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{})
-}
-
 func (m *Repository) AdminPostReservation(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
@@ -587,4 +584,18 @@ func (m *Repository) AdminPostReservation(w http.ResponseWriter, r *http.Request
 	m.App.Session.Put(r.Context(), "flash", "Changed is Saved")
 	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 
+}
+
+func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-reservations-calendar.page.tmpl", &models.TemplateData{})
+}
+
+// AdminProcessReservation: marks a reservation as processed
+func (m *Repository) AdminProcessReservation(w http.ResponseWriter, r *http.Request) {
+	id, _ := strconv.Atoi(chi.URLParam(r, "id"))
+	src := chi.URLParam(r, "src")
+	_ = m.DB.UpdateProcessedForReservation(id, 1)
+
+	m.App.Session.Put(r.Context(), "flash", "reservation marked as processed")
+	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 }
