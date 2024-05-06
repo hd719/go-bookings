771648904caacd1a267b51b2ac3879d868ac20c6
Hamel Desai <hameldesai93@gmail.com>
2024-05-06 13:12:58 -0400

    section-17-video-150

--- migrations/20240320234642_seed_restrictions_table.postgres.up.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO "public"."restrictions" ("id", "restriction_name", "created_at", "updated_at") VALUES
-(1, 'Reservation', '2024-03-10 00:00:00', '2024-03-10 00:00:00'),
-(2, 'Owner Block', '2024-03-20 00:00:00', '2024-03-20 00:00:00');
