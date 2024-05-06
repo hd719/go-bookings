0e8cc6168809398f9372f22c96f3d6810cac03e3
Hamel Desai <hameldesai93@gmail.com>
2024-05-06 13:12:54 -0400

    section-6-video-45-enabling-static-files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,12 +12,17 @@ import (
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+
 	mux.Use(middleware.Recoverer)
 	mux.Use(NoSurf)
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
+
+	// . is the root level directory of the app
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
