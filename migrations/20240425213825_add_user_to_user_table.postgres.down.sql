15cd948d3c0ae149e7ebe13edb7a1afa2e267ef2
Hamel Desai <hameldesai93@gmail.com>
2024-05-06 13:13:00 -0400

    section-19-video-171

---
+++ migrations/20240425213825_add_user_to_user_table.postgres.down.sql
@@ -0,0 +1,1 @@
+INSERT INTO "public"."users" ("first_name", "last_name", "email", "password", "access_level", "created_at", "updated_at") VALUES ('admin', 'admin', 'admin@admin.com', '$2a$14$ajq8Q7fbtFRQvXpdCq7Jcuy.Rx1h/L4J60Otx.gyNLbAYctGMJ9tK', 3, '2024-04-04 00:00:00', '2024-04-04 00:00:00');
